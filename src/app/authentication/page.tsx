c24a2f5b490db931a4c5448974f10a6b30f3fa68
Christian Martins <cmts2010@gmail.com>
2025-06-02 22:34:22 -0300

    feat: adicionar lógica de autenticação e botão de logout na página do dashboard

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,13 +1,21 @@
-"use client";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import SignUpForm from "./components/sign-up-form";
+import { auth } from "@/lib/auth";
+
 import LoginForm from "./components/login-form";
+import SignUpForm from "./components/sign-up-form";
 
-export default function AuthenticationPage() {
-
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
@@ -22,4 +30,6 @@ export default function AuthenticationPage() {
       </Tabs>
     </div>
   );
-}
+};
+
+export default AuthenticationPage;
