17b97ac12af26b9b84a07793c4b46cd097c70ed0
Christian Martins <cmts2010@gmail.com>
2025-06-03 11:31:12 -0300

    feat: add UI components for Avatar, Dropdown Menu, Separator, Sheet, Sidebar, Skeleton, and Tooltip
    - Implemented Avatar component with image and fallback support.
    - Created Dropdown Menu component with various subcomponents for items, groups, and triggers.
    - Added Separator component for visual separation in layouts.
    - Developed Sheet component for modal-like overlays with customizable content.
    - Introduced Sidebar component with context management for open/closed states and mobile responsiveness.
    - Added Skeleton component for loading placeholders.
    - Implemented Tooltip component for displaying additional information on hover.
    - Included utility hooks for mobile detection.

---
+++ src/app/authentication/_components/login-form.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import ButtonGoogle from "./button-google";
+
+const LoginForm = () => {
+  const router = useRouter();
+  const loginSchema = z.object({
+    email: z
+      .string()
+      .trim()
+      .min(1, { message: "O email é obrigatório" })
+      .email({ message: "Email inválido" }),
+    password: z
+      .string()
+      .trim()
+      .min(8, { message: "A senha deve ter pelo menos 8 caracteres" }),
+  });
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+    try {
+      await authClient.signIn.email(
+        {
+          email: values.email,
+          password: values.password,
+        },
+        {
+          onSuccess: () => {
+            router.push("/dashboard");
+          },
+          onError: (ctx) => {
+            toast.error("E-mail ou senha inválidos.");
+          },
+        }
+      );
+    } catch (error) {
+      toast.error("Erro inesperado. Tente novamente.");
+    }
+  }
+
+  const handleGoogleLogin = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+      scopes: ["email", "profile"],
+    });
+  };
+
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Preencha para fazer login.</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Digite sua senha"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <div className="w-full space-y-2">
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  "Entrar"
+                )}
+              </Button>
+              <ButtonGoogle handleGoogleLogin={handleGoogleLogin} />
+            </div>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export default LoginForm;
